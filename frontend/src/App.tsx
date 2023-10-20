2fb1bcccc85fb7bf187d940eba6eb8491a3a349c
Kamui2134 <ctorkey@gmail.com>
2023-10-20 22:05:44 +0300

    Добавление router сайта и компонентов которые он будет соединять

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import {RouterProvider} from 'react-router-dom'
+import router from './router'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+      <div className="App">
+        <RouterProvider router={router}/>
+      </div>
   )
 }
 
---
+++ frontend/src/router.tsx
@@ -0,0 +1,31 @@
+import Registration from "./components/Registration/Registration";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import Profil from "./components/Profil/Profil";
+import Turnirs from "./components/Turnirs/Turnirs";
+import {createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: "/home",
+        element: <Home/>,
+    },
+    {
+        path: "/",
+        element: <Registration/>,
+    },
+    {
+        path: "/login",
+        element: <Login/>,
+    },
+    {
+        path: "/profil",
+        element: <Profil/>,
+    },
+    {
+        path: "/turnirs",
+        element: <Turnirs/>,
+    },
+])
+
+export default router;
