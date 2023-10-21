8203f040d2512f3316359cbefd47ceb0cb0ac32b
Toropigeon <71664787+TheAnton1@users.noreply.github.com>
2023-10-21 13:01:56 +0300

    Home page init

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import profil from "../Profil/Profil.tsx";
 
 const Home = () => {
     return (
         <div>
-            
+            <button>
+                <Link to={"/profil"}> Профиль </Link>
+            </button>
+            <button>
+                <Link to={"/turnirs"}> Список турниров </Link>
+            </button>
         </div>
     );
 };
