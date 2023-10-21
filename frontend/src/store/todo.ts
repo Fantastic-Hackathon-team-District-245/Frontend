7f5ebb651aa0bc37ec3bcb59b114717b88b45470
Kamui2134 <ctorkey@gmail.com>
2023-10-21 17:21:38 +0300

    Убрал комментарии

--- frontend/src/store/todo.ts
+++ frontend/src/store/todo.ts
@@ -14,9 +14,6 @@ class Todo {
     removeTodo(id) {
        this.todos = this.todos.filter(todo => todo.id != id);
     }
-    // completeTodo(id) {
-    //     this.todos = this.todos.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo);
-    // }
 }
 
 export default new Todo()
